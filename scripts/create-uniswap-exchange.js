5ea3aa8263c537da222cfc4b5dfb3e91626f8846
Haythem Sellami <sellamihaythem96@gmail.com>
2020-04-27 16:39:47 +0100

    add liquidity to uniswap

--- scripts/create-uniswap-exchange.js
+++ scripts/create-uniswap-exchange.js
@@ -1,6 +1,10 @@
+/**
+ * script to create a uniswap exchange and add liquidity into it
+ */
 const config = require("./config.json");
 
 const UniswapFactory = artifacts.require("UniswapFactoryInterface.sol");
+const UniswapExchange = artifacts.require("UniswapExchangeInterface.sol");
 
 module.exports = async function(callback) {
     try {
@@ -13,18 +17,36 @@ module.exports = async function(callback) {
             return;
         }
 
+        // get uniswap factory
         let uniswapFactory = await UniswapFactory.at(config.create_uniswap_exchange.uniswap_factory);
         console.log("Uniswap Factory: ", uniswapFactory.address);
 
+        // create exchange
         await uniswapFactory.createExchange(
             config.create_uniswap_exchange.oToken_address
         );
 
+        // get exchange instance
         let createdExchangeAddress = await uniswapFactory.getExchange(
             config.create_uniswap_exchange.oToken_address
         );
 
-        console.log("Created exchange: ", createdExchangeAddress);
+        let uniswapExchange = await UniswapExchange.at(createdExchangeAddress);
+
+        console.log("Created exchange: ", uniswapExchange.address);
+
+        // add liquidity
+        const oneMonthInSeconds = 60 * 60 * 24 * 30;
+        const now = new Date().getTime() / 1000;
+      
+        await uniswapExchange.addLiquidity(
+            0,
+            config.create_uniswap_exchange.token_liquidity,
+            parseInt(now + oneMonthInSeconds),
+            { 
+                value: web3.utils.toWei(config.create_uniswap_exchange.eth_liquidity, 'ether')            
+            }
+        );
 
         callback();
     }
