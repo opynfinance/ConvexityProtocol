2fcf64f92ba8e928a93acba7b1eb27760032a45f
Anton Cheng <antonassocareer@gmail.com>
2020-08-06 23:27:49 +0800

    Put calculateMaxOptionsToCreate in helper

--- test/utils/helper.ts
+++ test/utils/helper.ts
@@ -6,3 +6,15 @@ export function checkVault(
   expect(vault['0'].toString()).to.equal(expectedCollateral);
   expect(vault['1'].toString()).to.equal(oTokensIssued);
 }
+
+export function calculateMaxOptionsToCreate(
+  collateral: number,
+  collateralToStrikePrice: number,
+  minCollateralizationRatio: number,
+  strikePrice: number
+): number {
+  return Math.floor(
+    (collateral * collateralToStrikePrice) /
+      (minCollateralizationRatio * strikePrice)
+  );
+}
