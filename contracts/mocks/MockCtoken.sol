434e20b30fac3c567c6d4c2ed269326cccf8c069
Anton Cheng <antonassocareer@gmail.com>
2020-08-19 20:19:36 +0800

    Task/ctoken collateral test (#179)
    * Edit MockCtoken to inherit MockERC20
    
    * Add compound collateral test
    
    * Update solcover.js

--- contracts/mocks/MockCtoken.sol
+++ contracts/mocks/MockCtoken.sol
@@ -1,13 +1,19 @@
 pragma solidity 0.5.10;
 
 import "../interfaces/OracleInterface.sol";
+import "./MockERC20.sol";
 
 
-contract MockCtoken {
+contract MockCtoken is MockERC20 {
     address public underlying;
     uint256 public exchangeRateStored;
 
-    constructor(address _underlying, uint256 _exchageRateStored) public {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _underlying,
+        uint256 _exchageRateStored
+    ) public MockERC20(_name, _symbol, 8) {
         underlying = _underlying;
         exchangeRateStored = _exchageRateStored;
     }
