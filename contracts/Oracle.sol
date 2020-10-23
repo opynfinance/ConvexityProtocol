559a72f7a3cec50858c8969652f45e47f3142ddb
mshrieve <mshrieve@gmail.com>
2020-10-22 19:32:27 -0700

    removed hard-coded ceth

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -22,8 +22,6 @@ contract Oracle is Ownable {
 
     constructor(address _oracleAddress) public {
         priceOracle = CompoundOracleInterface(_oracleAddress);
-        // Mainnet
-        cEth = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
     }
 
     event CtokenUpdated(address indexed ctoken, bool isCtoken);
